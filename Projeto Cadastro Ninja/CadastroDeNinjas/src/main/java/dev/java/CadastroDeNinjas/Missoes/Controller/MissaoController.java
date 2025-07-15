e296b0260f4b1cc313a973660a440e3d52e2dd1e
AquilaDavid <aquiladavid07@gmail.com>
2025-07-15 15:22:44 -0300

    adicionado os get e post

--- Projeto Cadastro Ninja/CadastroDeNinjas/src/main/java/dev/java/CadastroDeNinjas/Missoes/Controller/MissaoController.java
+++ Projeto Cadastro Ninja/CadastroDeNinjas/src/main/java/dev/java/CadastroDeNinjas/Missoes/Controller/MissaoController.java
@@ -2,17 +2,43 @@ package dev.java.CadastroDeNinjas.Missoes.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import dev.java.CadastroDeNinjas.Missoes.DTO.MissaoDTO;
+import dev.java.CadastroDeNinjas.Missoes.DTO.MissaoResponseveDTO;
+import dev.java.CadastroDeNinjas.Missoes.Model.MissaoModel;
+import dev.java.CadastroDeNinjas.Missoes.Service.MissaoService;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 
 @RestController
-@RequestMapping
+@RequestMapping("Missoes")
+@RequiredArgsConstructor
 public class MissaoController {
     
+    private final MissaoService missaoService;
+
     @GetMapping("/BemVindoMissao")
     public String Missao() {
         return "Você chegou as missões";
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> salvarMissao(@RequestParam MissaoDTO dto) {
+        missaoService.salvarMissao(dto);
+        return ResponseEntity.ok().build();
+    }
+    
+    @GetMapping
+    public ResponseEntity<MissaoResponseveDTO> buscarMissao(@RequestParam Long id) {
+        MissaoModel model = missaoService.buscarMissao(id);
+        MissaoResponseveDTO response = new MissaoResponseveDTO(model.getNome(), model.getClassificacao(), false);
+        return ResponseEntity.ok(response);
     }
     
 
